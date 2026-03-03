fdbed83cb4de08a72bece5dfbcac6ad495cc4c69
dipthi0780 <dipthid28@gmail.com>
2026-03-03 18:25:58 +0530

    Add files via upload

---
+++ variables1.py
@@ -0,0 +1,62 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+a=23
+print(a)
+23
+b=34
+print(b)
+34
+x=34
+print(X)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    print(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+print(x)
+34
+#python is case sensitive
+#two variables at a time
+a=10,b=40
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=45;b=57
+print(a*b)
+2565
+a2=10
+print(a2)
+10
+a0123456=8754678056
+print(a0123456)
+8754678056
+name="dipthi"
+print(name)
+dipthi
+_a=40
+print(_a)
+40
+>>> _=467
+>>> print(_)
+467
+>>> #del
+>>> a=10
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a2'?
+>>> mail="dipthi@gmail.com"
+>>> print(mail)
+dipthi@gmail.com
+>>> First_name="dipthi"
+>>> Last_name="reddy"
+>>> print(First_name+" "+Last_name)
+dipthi reddy
+>>> x=30
+>>> print(x)
+30
+>>> y=60
+>>> print(x)
+30
+>>> if=80
+SyntaxError: invalid syntax
