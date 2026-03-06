cc1d60368e1b42c2f437e6da3e36d0e5f75f3e0d
dipthi0780 <dipthid28@gmail.com>
2026-03-06 21:29:15 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,307 @@
+#if condition by using comparision operators
+'''a=7
+b=8
+if a<b:
+    print("true")'''
+
+
+'''a=7
+b=8
+if a>b:
+    print("true")'''
+
+
+'''a=7
+b=8
+if a!=b:
+    print("true")'''
+
+'''a=10
+b=80
+if a<=b:
+    print("true")'''
+
+'''a=7
+b=8
+if a>=b:
+    print("true")'''
+
+
+
+'''a=7
+b=10
+if a==b:
+    print("equal")'''
+
+'''a=40
+b=40
+if a==b:
+    print("equal")'''
+
+'''a="java"
+b="python"
+if a==b:
+    print("true")'''
+
+'''a="python"
+b="python"
+if a==b:
+    print("true")'''
+
+#run-time:
+'''a=int(input("value of a:"))
+b=int(input("value of b:"))
+if a==b:
+    print("sucess")'''
+
+'''a=int(input("value of a"))
+if a>100:
+    print("greater")'''
+
+#logical operators:
+'''a=4
+b=6
+if a<b and b<a:
+    print("true")'''
+
+'''a=4
+b=6
+if a!=b and b<a:
+    print("true)'''
+
+'''a=4
+b=6
+if a!=b and b==a:
+    print("true")'''
+
+'''a=4
+b=6
+if b!=a or a==b:
+    print("true")'''
+
+
+'''a=7
+b=5
+if a>b or a!=b:
+    print("true")'''
+
+
+'''a=7
+b=5
+if a==b or a!=b:
+    print("true")'''
+
+'''a=5
+b=8
+if not a>b:
+    print("true")'''
+
+'''a=5
+b=8
+if not a>b and a!=b:
+    print("true")'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b and b>a:
+    print("sucess")'''
+
+'''a=input("data1")
+b=input("data2")
+if a=="python" and b=="java":
+    print("done")'''
+
+'''a=input("data1")
+b=input("data2")
+if a==a and b==b:
+    print("done")'''
+
+#identify:
+'''a=3
+if type(a) is int:
+    print("yes")'''
+
+'''a=3
+if type(a) is not int:
+    print("yes")'''
+        
+'''a=3.4
+if type(a) is not int:
+    print("yes")'''
+
+'''a=3.8
+if type(a) is float:
+    print("yes")'''
+
+'''a=input("a value")
+if type(a) is str:
+    print("done")'''
+
+'''a=complex(input("value"))
+if type(a) is complex:
+    print("done")'''
+
+'''a=bool(input("value"))
+if type(a) is bool:
+    print("done")'''
+
+#membership:
+'''a=[2,3,4,5,6,7,8,9]
+if 8 in a:
+    print("true")'''
+
+
+'''a=[2,3,4,5,6,7,8,9]
+if 8 not in a:
+    print("true")'''
+
+'''a=[1,2,4,5,6,7,8,9,10]
+b=int(input("value"))
+if b in a:
+    print("done")'''
+
+#if_else in comparison:
+'''a=3
+b=5
+if a<b and b>a:
+    print("true")
+else:
+    print("false")'''
+
+
+'''a=3
+b=5
+if a!=b and b==a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=3
+b=5
+if a<b or b==a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=3
+if type(a)is int:
+    print("true")
+else:
+    print("no")'''
+
+'''a=3
+if type(a)is not int:
+    print("true")
+else:
+    print("no") '''
+
+'''a=3,4,5,6,7,8,9
+if 8 is in a:
+    print("true")
+else:
+    print("no")'''
+
+'''a=3,4,5,6,7,8,9
+if 8 is not a:
+    print("true"):
+else:
+    print("no")'''
+
+#if_elif:
+'''a=4
+b=5
+if a!=b:
+    print("not equal")
+elif a==b:
+    print("equal")
+
+a=4
+b=5
+if a==b:
+    print("not equal")
+elif a!=b:
+    print("equal")
+
+a=14;b=6
+if a<b and b<a:
+    print("yes")
+elif a!=b or a==b:
+    print("no")
+
+a=4
+if type(a) is int:
+    print("true")
+elif type(a) is float:
+    print("false")
+
+
+a=1,2,3,4,5,6,7,8,9
+if 43 in a:
+    print("there")
+elif 4 is not a:
+    print("no")'''
+
+#if_elif_else:
+'''a=10;b=20
+if a<b:
+    print('yes')
+elif b>a:
+    print("no")
+else:
+    print("not")
+
+a=10;b=20
+if a>b:
+    print('yes')
+elif b!=a:
+    print("no")
+else:
+    print("not")
+
+a=5
+b=5
+if a<b and b>a:
+    print("y")
+elif a!=b or a==b:
+    print("n")
+else:
+    print("false")
+
+a=4.5
+if type(a) is int:
+    print("y")
+elif type(a) is float:
+    print('n')
+else:
+    print("none")
+
+a=1,2,3,4,5,6,7,8,9
+if 56 is a:
+    print("y")
+elif 54 is not a:
+    print("n")
+else:print('none')'''
+
+#multiple_if: used when all condition true peinr all:
+'''a=9
+b=12
+if a<b:
+    print('true')
+if b>a:
+    print('false')
+if a!=b:
+    print("done")'''
+          
+          
+
+
+        
+
+
+        
+
+
+
+
+
