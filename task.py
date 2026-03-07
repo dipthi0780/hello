c38bb4b666699b890d0d6d95cae7b35db8cca980
dipthi0780 <dipthid28@gmail.com>
2026-03-07 20:54:37 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,68 @@
+'''age=int(input("age"))
+if age>=18:
+    print("eligible")
+else:
+    print("not eligible")'''
+
+
+'''num=int(input("enter value:"))
+if num%2==0:
+    print("even")
+else:
+    print("odd")'''
+
+'''ch=input("enter a character:")
+if ch in ["a,e,i,o,u"]:
+    print("vowel")
+else:
+    print("consonent")'''
+
+'''a=["a,e,i,o,u"]
+ch=input("enter char")
+if ch in a:
+    print("vowel")
+else:
+    print("consonent")'''
+
+'''a=int(input("enter year"))
+if a %4==0:
+    print("leap year")
+else:
+    print("not leap year")'''
+
+'''a=["dipthi"]
+name=input("enter name:")
+if name in a:
+    print("welcome dipthi")
+elif name not in a:
+        print("welcome guest")'''
+
+'''a=["dipthi","devi","rupa","cherry","racheal"]
+name=input("enter name")
+if name in a:
+    print("welcome",name)
+else:
+        print("welcome guest")'''
+
+'''username=input("enter username")
+password=input("enter password")
+if username=="dipthi":
+    if password=='20034':
+        print("login successfull")
+    else:
+        print("incorrect password")
+else:
+    print("login failed")'''
+
+
+'''username=input("enter username")
+password=input("enter password")
+if username=="dipthi" and password=='200@34':
+        print("login successfull")
+else:
+    print("login failed")'''
+
+
+
+
+      
