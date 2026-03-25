1b44afcb91bc549d954f6dcd6b790761cfb3b88b
dipthi0780 <dipthid28@gmail.com>
2026-03-25 11:20:27 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,116 @@
+'''a=10
+b=20
+print("the sum is:",a+b)
+print("the difference is:",a-b)
+print("the product is:",a*b)
+a=100;b=200
+print("the sum is:",a+b)
+print("the difference is:",a-b)
+print("the product is:",a*b)
+a=1000;b=2000
+print("the sum is:",a+b)
+print("the difference is:",a-b)
+print("the product is:",a*b)'''
+
+#function:
+'''def calculate(a,b):
+    print("the sum is:",a+b)
+    print("the difference is:",a-b)
+    print("the product is:",a*b)
+calculate(10,20)
+calculate(100,200)
+calculate(1000,2000)'''
+
+'''def calculate(a,b):
+    print("the modulo div is:",a%b)
+    print("the power is:",a**b)
+    print("the integer div is:",a//b)
+calculate(10,5)
+calculate(4,2)'''
+
+'''def add(a,b):
+    print(a+b)
+add(6,8)'''
+
+#runtime:
+'''def add():
+    a=int(input("a value:"))
+    b=int(input("b vlaue:"))
+    print(a*b)
+add()'''
+
+'''while True:
+    def fullname():
+        fname=input("first name")
+        lname=input("last name")
+        print((fname+" "+lname).title())
+    fullname()'''
+
+#print vs return:
+'''def mul(a,b):
+    print(a*b)
+mul(4,5)'''
+
+'''def mul(a,b):
+    return a*b
+print(mul(4,5))'''
+
+'''def cal(a,b):
+    c=a+b
+    d=a-b
+    e=a*b
+    print(c)
+    print(d)
+    print(e)
+cal(4,5)'''
+
+'''def cal(a,b):
+    c=a+b
+    d=a-b
+    e=a*b
+    #return(c)
+    #return(d)
+    #return(e)
+    return c,d,e
+print(cal(4,5))'''
+
+#task:
+'''def bill():
+    a=int(input("enter no of friends:"))
+    b=int(input("enter amount:"))
+    #perhead=amount/a
+    #print(perhead)
+    print("perhead bill is",b//a)
+bill()'''
+
+'''def bill():
+    a=int(input("enter no of friends:"))
+    b=int(input("enter amount:"))
+    print("perhead amount {}".format(b//a))
+    print(f"bill is {b//a}")
+bill()'''
+
+#task:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
