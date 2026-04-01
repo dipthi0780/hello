e3106499838adce6f3e72a81e332752ed7c5fca0
dipthi0780 <dipthid28@gmail.com>
2026-04-01 20:29:54 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,82 @@
+#right angle traingle:
+'''n=5
+for i in range(1,n+1):
+    for j in range(i):
+        print("*",end=" ")
+    print()'''
+
+#reverse:
+'''n=5
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()'''
+
+#square:
+'''n=4
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()'''
+
+#pyramid:
+'''n=5
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end=" ")
+    print()'''
+
+#diamond:
+'''n=3
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for j in range(2*i-1):
+        print("*",end="")
+    print()
+for i in range(n-1,0,-1):
+    for j in range(n-i):
+        print(" ",end="")
+    for j in range(2*i-1):
+        print("*",end="")
+    print()'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
